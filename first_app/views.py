c25fbc35339528ca9ffa2261a2f8631699d6b346
Jun C <kjstudios858@gmail.com>
2018-10-19 00:17:54 -0700

    refactor Added table to display information from querie search from Table

--- first_app/views.py
+++ first_app/views.py
@@ -9,3 +9,5 @@ def index(request):
     webpages_list = AccessRecord.objects.order_by('date')
     date_dict = {'access_records':webpages_list}
     return render(request, 'first_app/index.html', context=date_dict)
+
+
